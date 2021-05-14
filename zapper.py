96fe65213b8c004e05ea335cc3fabf1f3a7818ca
Antony Karounis <nullfocus@gmail.com>
2021-05-14 15:01:55 -0400

    added ability to run python files

--- zapper.py
+++ zapper.py
@@ -17,12 +17,26 @@ def load_files():
 
 def run_script(fullname):
     start = timer()
-    output = subprocess.run([fullname], check=True, capture_output=True)
+
+    fullname = str(fullname)
+
+    output = ''
+
+
+    if fullname.endswith('.py'):
+        script_locals = {'args' : request.args, 'response' : '...'}
+
+        with open(fullname) as file:
+            exec(file.read(), None, script_locals)
+        
+        output = script_locals['response']
+        
+    else:
+        output = subprocess.run([fullname], check=True, capture_output=True).stdout.decode("utf-8") 
+
     timespan = timer() - start
 
-    decoded_output = output.stdout.decode("utf-8") 
-
-    return decoded_output, timespan
+    return output, timespan
 
 def page(title, content):
     return f'''
@@ -30,9 +44,35 @@ def page(title, content):
     <head>
         <title>{title}</title>
         <style>
-            body{{ font-family:monospace }}
-            pre{{ font-size: 8pt }}
-            ul{{ list-style: decimal;  }}
+            html, body, div, ul, li, a {{
+                font-family: monospace;
+                margin: 0;
+                padding: 0;
+                border: 0;
+            }}
+
+            html{{ font-size: 60%; padding: 1em;}}
+
+            @media screen 
+                and (min-width: 0px)
+                and (max-width: 300px) 
+            {{ html {{ font-size: 3.5em; }} }}
+
+            @media screen 
+                and (min-width: 301px)
+                and (max-width: 600px) 
+            {{ html {{ font-size: 3em; }} }}
+
+            @media screen 
+                and (min-width: 601px)
+                and (max-width: 1000px) 
+            {{ html {{ font-size: 2.5em; }} }}
+
+            @media screen 
+                and (min-width: 1001px)
+            {{ html {{ font-size: 2em; }} }}
+
+            ul{{ list-style: decimal; margin-left: 2em}}
         </style>
     </head>
     <body>
@@ -83,6 +123,11 @@ def run():
 <pre>{script_output}</pre>
 ''')
 
+@app.route('/favicon.ico')
+@logged
+def favicon():
+    return '', 404
+
 #https://blog.sneawo.com/blog/2017/12/20/no-cache-headers-in-flask/
 @app.after_request
 def set_response_headers(response):
@@ -97,9 +142,12 @@ def all_exception_handler(error):
     return 'Error', 500
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, filename='log.txt')
+    filepath = os.path.dirname(os.path.realpath(__name__))
+
+    logfile_path = filepath + '/log.txt'
+    scripts_path = filepath + '/scripts/'
+
+    logging.basicConfig(level=logging.DEBUG, filename=logfile_path)
     log = logging.getLogger(os.path.basename(__file__))
     
-    scripts_path = os.path.dirname(os.path.realpath(__name__)) + '/scripts/'
-
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
